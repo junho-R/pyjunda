d8d3a1aa03b84df3289984e75b788d9bc9ef5ab1
junho-R <55642452+junho-R@users.noreply.github.com>
2019-10-13 12:40:34 +0900

    Update FTPTEST.py

--- FTPTEST.py
+++ FTPTEST.py
@@ -1,1 +1,7 @@
-test.py
+실행창 : taskschd.msc
+마우스 오른쪽 클릭 : 기본작업 만들기
+프로그램/스크립트 : c:\Python\pyton.exe
+인수추가(옵션)(A) : c:\python\code\ftp_upload_final.py
+    
+ 만약 실행시 콘솔에 뜨지 않고 백그라운드(스탤스) 모드로 실행하고 싶다면
+실행할 명령어를 c:\Python\pythonw.exe
